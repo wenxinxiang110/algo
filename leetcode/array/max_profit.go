722c4377de14c0d37d1ab3b1d9d7d2cfc1a57e5f
wenxinxiang <wenxinxiang@tencent.com>
2020-07-06 00:15:25 +0800

    a little change

--- leetcode/array/max_profit.go
+++ leetcode/array/max_profit.go
@@ -26,10 +26,7 @@ func maxProfit(prices []int) int {
 //设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
 //
 //注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
-//
-//来源：力扣（LeetCode）
 //链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii
-//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 func maxProfitII(prices []int) int {
 	all := 0
 	for i := 1; i < len(prices); i++ {
