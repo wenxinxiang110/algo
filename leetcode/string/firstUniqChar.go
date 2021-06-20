8b695adf3c3b9dffb12503a3afade0bdab80c4f0
wenxinxiang <wenxinxiang@tencent.com>
2021-06-21 01:26:10 +0800

    refactor(easy.387): 按照官方思路1实现，时间最优，内战占用较高

--- leetcode/string/firstUniqChar.go
+++ leetcode/string/firstUniqChar.go
@@ -1,9 +1,5 @@
 package string
 
-import (
-	"strings"
-)
-
 //给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
 //示例：
 //
@@ -17,13 +13,23 @@ import (
 //提示：你可以假定该字符串只包含小写字母。
 
 func firstUniqChar(s string) int {
+	return firstUniqCharDoubleRange(s)
+}
 
+// 思路1: 第一次遍历，储存出现次数；第二次遍历，发现是只出现一次的，直接返回索引
+func firstUniqCharDoubleRange(s string) int {
+	// unique标记每个字符初始出现时候的索引
+	var unique = [26]int{}
 	array := []rune(s)
+
 	for i := 0; i < len(array); i++ {
-		if strings.IndexRune(s, array[i]) == strings.LastIndexAny(s, string(array[i])) {
+		unique[array[i]-'a']++
+	}
+
+	for i := 0; i < len(array); i++ {
+		if unique[array[i]-'a'] == 1 {
 			return i
 		}
 	}
-
 	return -1
 }
