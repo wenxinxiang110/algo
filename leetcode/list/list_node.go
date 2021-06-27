23c5fcaff882099053502ba1ec1c1f134c660d2b
wenxinxiang <wenxinxiang@tencent.com>
2021-06-27 19:03:43 +0800

    feat(list): 完善链表包的方法，方便单元测试

--- leetcode/list/list_node.go
+++ leetcode/list/list_node.go
@@ -1,7 +1,7 @@
 package list
 
 import (
-	"log"
+	"bytes"
 	"strconv"
 	"strings"
 )
@@ -13,56 +13,67 @@ import (
  *
  */
 
-//Definition for singly-linked list.
+const (
+	// 链表表达式默认分割符
+	defaultSep = "->"
+)
+
+// ListNode Definition for singly-linked list.
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+func (l *ListNode) String() string {
+	if l == nil {
+		return ""
+	}
+
+	res := new(bytes.Buffer)
+	res.WriteString(strconv.Itoa(l.Val))
+	cur := l.Next
+	for cur != nil {
+		res.WriteString(defaultSep)
+		res.WriteString(strconv.Itoa(cur.Val))
+		cur = cur.Next
+	}
+
+	return res.String()
+}
+
+// NewListNode 构造函数
 func NewListNode(val int) *ListNode {
 	return &ListNode{Val: val}
 }
 
-/**
- * 对@ParseListNode方法的简单封装
- *
- * param: string listExp
- * return: *ListNode
- */
+// SimpleParse 对ParseListNode方法的简单封装，使用默认分隔符解析链表
+// param: listExp 链表的表达式
+// return: 头结点
 func SimpleParse(listExp string) *ListNode {
-	return ParseListNode(listExp, "->")
+	return ParseListNode(listExp, defaultSep)
 }
 
-/**
- * 根据表达式和分割符生成链表
- * 例如：
- * 1->1->2->3->3
- * 1->1->2
- *
- * param: string exp 链表表达式
- * param: string spt 表达式的分割符
- * return: *ListNode 生成的链表的头节点
- */
+// ParseListNode 根据表达式和分割符生成链表
+// 例如：
+// 1->1->2->3->3
+// 1->1->2
+// param: exp 链表表达式
+// param: spt 表达式的分割符
+// return: 生成的链表的头节点
 func ParseListNode(exp, spt string) *ListNode {
-
-	// 一个没有用的假的头节点，这样遇到真正的第一个节点的时候就不用特殊处理了
-	head := NewListNode(0)
-
-	// 当前处理的节点
-	var cur = head
-
-	// 节点值
-	vals := strings.Split(exp, spt)
-
-	for _, val := range vals {
-		value, err := strconv.Atoi(val)
-
-		// todo: 出错处理暂时不弄，直接返回nil
-		if err != nil {
-			log.Fatal(err)
-			return nil
+	var (
+		// 一个没有用的假的头节点，这样遇到真正的第一个节点的时候就不用特殊处理了
+		head = NewListNode(0)
+		// 当前处理的节点
+		cur = head
+		// 节点值
+		val = strings.Split(exp, spt)
+	)
+	for _, v := range val {
+		if v == "" {
+			continue
 		}
-
+		value, _ := strconv.Atoi(v)
 		cur.Next = NewListNode(value)
 		cur = cur.Next
 	}
@@ -70,13 +81,7 @@ func ParseListNode(exp, spt string) *ListNode {
 	return head.Next
 }
 
-/**
- * 判断两个链表是否相等
- *
- * param: *ListNode first 第一个链表的头节点
- * param: *ListNode second 第二个链表的头节点
- * return: bool
- */
+// Equal 判断两个链表是否相等
 func Equal(first, second *ListNode) bool {
 	for first != nil && second != nil {
 
--- leetcode/list/list_node_test.go
+++ leetcode/list/list_node_test.go
@@ -16,6 +16,14 @@ func TestParseListNode(t *testing.T) {
 		want *ListNode
 	}{
 		{
+			"nil",
+			args{
+				exp: "",
+				spt: "->",
+			},
+			nil,
+		},
+		{
 			"",
 			args{
 				exp: "1->1->2",
@@ -57,6 +65,35 @@ func TestParseListNode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ParseListNode(tt.args.exp, tt.args.spt); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ParseListNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestListNode_String(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		linkExp string
+	}{
+		// TODO: Add test cases.
+		{
+			"nil",
+			"",
+		},
+		{
+			"normal",
+			"1->1->2",
+		},
+		{
+			"normal",
+			"1->1->2->3->3",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SimpleParse(tt.linkExp).String(); got != tt.linkExp {
+				t.Errorf("String(%v) = %v", tt.linkExp, got)
 			}
 		})
 	}
