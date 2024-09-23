94fd52a6a36a926e0ae4a9588efa4a75c40ad4fd
wenxinxiang <wenxinxiang@tencent.com>
2024-09-23 10:22:20 +0800

    feat(esy.202): isHappy快乐数
    「快乐数」 定义为：
    
    对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
    然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
    如果这个过程 结果为 1，那么这个数就是快乐数。

---
+++ leetcode/editor/cn/202_happy-number_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_isHappy(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "19",
+			args: args{
+				n: 19,
+			},
+			want: true,
+		}, {
+			name: "2",
+			args: args{
+				n: 2,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isHappy(tt.args.n); got != tt.want {
+				t.Errorf("isHappy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
