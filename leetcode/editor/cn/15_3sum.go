817aff80dd418ca42693962fb13ac652af5fe7f8
wenxinxiang <wenxinxiang@tencent.com>
2024-09-25 11:15:18 +0800

    feat(medium.15): threeSum三数之和
    给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。

---
+++ leetcode/editor/cn/15_3sum.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"sort"
+)
+
+//给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j !=
+//k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+//
+// 注意：答案中不可以包含重复的三元组。
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+//解释：
+//nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+//nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+//nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+//不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+//注意，输出的顺序和三元组的顺序并不重要。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [0,1,1]
+//输出：[]
+//解释：唯一可能的三元组和不为 0 。
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [0,0,0]
+//输出：[[0,0,0]]
+//解释：唯一可能的三元组和为 0 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 3 <= nums.length <= 3000
+// -10⁵ <= nums[i] <= 10⁵
+//
+//
+// Related Topics 数组 双指针 排序 👍 7068 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func threeSum(nums []int) (result [][]int) {
+	// 先排序
+	sort.Ints(nums)
+
+	// 再搜索
+	for i := 0; i < len(nums)-1; i++ {
+		// 去重a,而且同样的结果再搜索一遍没有意义
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		var left, right = i + 1, len(nums) - 1
+		for left < right {
+			if nums[i]+nums[left]+nums[right] < 0 {
+				left++
+			} else if nums[i]+nums[left]+nums[right] > 0 {
+				right--
+			} else {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			}
+		}
+	}
+	return
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
