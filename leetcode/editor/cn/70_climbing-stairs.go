d0850c1c8086cc0db86b5adc346d9f49597ad0b1
wenxinxiang <wenxinxiang@tencent.com>
2024-10-27 17:44:52 +0800

    feat(easy.70): climbStairs爬楼梯: 优化

--- leetcode/editor/cn/70_climbing-stairs.go
+++ leetcode/editor/cn/70_climbing-stairs.go
@@ -41,13 +41,11 @@ func climbStairs(n int) int {
 	if n <= 2 {
 		return n
 	}
-	dp := make([]int, n+1)
-	dp[1] = 1
-	dp[2] = 2
+	p, q := 1, 2
 	for i := 3; i <= n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
+		p, q = q, p+q
 	}
-	return dp[n]
+	return q
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
