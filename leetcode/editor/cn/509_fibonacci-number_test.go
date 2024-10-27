6f8e50d962552b22c3f1626f9e8403a5f0b8b14b
wenxinxiang <wenxinxiang@tencent.com>
2024-10-27 17:18:36 +0800

    feat(easy.509): fib斐波那契数

---
+++ leetcode/editor/cn/509_fibonacci-number_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_fib(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "normal",
+			args: args{3},
+			want: 2,
+		},
+		{
+			name: "normal",
+			args: args{4},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
