643251e97f0e27f5940559cb218cf9d8ec5f873d
wenxinxiang <wenxinxiang@tencent.com>
2024-10-27 17:26:46 +0800

    feat(easy.509): fib斐波那契数: 动态规划实现

--- leetcode/editor/cn/509_fibonacci-number.go
+++ leetcode/editor/cn/509_fibonacci-number.go
@@ -47,17 +47,13 @@ package main
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func fib(n int) int {
-	if n == 0 {
-		return 0
+	var dp = make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
 	}
-	if n <= 2 {
-		return 1
-	}
-	p, q := 1, 1
-	for i := 2; i < n; i++ {
-		p, q = q, p+q
-	}
-	return q
+	return dp[n]
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
