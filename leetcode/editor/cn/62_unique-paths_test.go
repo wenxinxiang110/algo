d08ab0b76fcc941b0275f14fc58c2691280755df
wenxinxiang <wenxinxiang@tencent.com>
2024-10-28 14:17:22 +0800

    feat(medium.62): uniquePaths 不同路径

---
+++ leetcode/editor/cn/62_unique-paths_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_uniquePaths(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{m: 3, n: 7},
+			want: 28,
+		},
+		{
+			name: "test2",
+			args: args{m: 3, n: 2},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
