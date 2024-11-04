6644e93ddf5effe1636a3264e4e48280707b5307
wenxinxiang <wenxinxiang@tencent.com>
2024-11-04 10:19:02 +0800

    feat): BasePackage基础背包问题，滚动数组实现

--- dp/package.go
+++ dp/package.go
@@ -1,34 +1,28 @@
 package dp
 
-import (
-	"fmt"
-)
-
 // BasePackage 基础背包问题
 // https://github.com/youngyangyang04/leetcode-master/blob/master/problems/%E8%83%8C%E5%8C%85%E7%90%86%E8%AE%BA%E5%9F%BA%E7%A1%8001%E8%83%8C%E5%8C%85-1.md
 func BasePackage(items []Item, maxWeight int) (maxValue int) {
 	if len(items) == 0 || maxWeight == 0 {
 		return 0
 	}
-	dp := make([][]int, len(items))
+	// 滚动数组实现，用一维数组存储压缩存储
+	// 因为这个背包每次其实只需要读取上一行的数据，所以其实没必要存储整个完整的 dp表格
+	dp := make([]int, maxWeight+1)
 	// 初始化
-	for i := range dp {
-		dp[i] = make([]int, maxWeight+1)
-	}
 	for i := items[0].Weight; i < maxWeight; i++ {
-		dp[0][i] = items[0].Value
+		dp[i] = items[0].Value
 	}
 	for i := 1; i < len(items); i++ {
-		for j := 1; j <= maxWeight; j++ {
-			if j < items[i].Weight {
-				dp[i][j] = dp[i-1][j]
-			} else {
-				dp[i][j] = max(dp[i-1][j], dp[i-1][j-items[i].Weight]+items[i].Value)
+		// 如果是滚动数组方式的，需要用倒序遍历，避免一个物品重复放入
+		for j := maxWeight; j > 0; j-- {
+			if j >= items[i].Weight {
+				dp[j] = max(dp[j], dp[j-items[i].Weight]+items[i].Value)
 			}
 		}
 	}
-	fmt.Println(dp)
-	return dp[len(items)-1][maxWeight]
+	//fmt.Println(dp)
+	return dp[maxWeight]
 }
 
 type Item struct {
