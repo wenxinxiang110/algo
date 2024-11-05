977e332877c2d458f19963430b364309da6722b4
wenxinxiang <wenxinxiang@tencent.com>
2024-11-05 14:17:18 +0800

    feat(medium.1049): lastStoneWeightII最后一块石头的重量II

---
+++ leetcode/editor/cn/1049_last-stone-weight-ii_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_lastStoneWeightII(t *testing.T) {
+	type args struct {
+		stones []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{
+				[]int{2, 7, 4, 1, 8, 1},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lastStoneWeightII(tt.args.stones); got != tt.want {
+				t.Errorf("lastStoneWeightII() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
