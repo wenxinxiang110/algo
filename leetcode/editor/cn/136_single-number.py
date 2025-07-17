8c18164f0c86397ec7f912419fdefbb4fdd9d2c7
wenxinxiang110 <DrXinhuanan@163.com>
2025-07-17 10:44:10 +0800

    feat(easy.136): (py) singleNumber 只出现一次的数字：优化实现

--- leetcode/editor/cn/136_single-number.py
+++ leetcode/editor/cn/136_single-number.py
@@ -1,6 +1,8 @@
 # package main
+from functools import reduce
 
-# 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
+
+# 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
 # 
 #  你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。 
 # 
@@ -54,8 +56,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        r = 0
-        for n in nums:
-            r ^= n
-        return r
+
+        return reduce(lambda x, y: x ^ y, nums)
 # leetcode submit region end(Prohibit modification and deletion)
