a7d122933f90607059146c10be80c20358dc660f
wenxinxiang <wenxinxiang@tencent.com>
2026-02-03 00:04:40 +0800

    feat(easy.283): moveZero 移动零

---
+++ leetcode/editor/cn/283_move-zeroes_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test1",
+			args: args{
+				nums: []int{1},
+			},
+			want: []int{1},
+		},
+		{
+			name: "test1",
+			args: args{
+				nums: []int{0, 1},
+			},
+			want: []int{1, 0},
+		},
+		{
+			name: "test2",
+			args: args{
+				nums: []int{0, 1, 2},
+			},
+			want: []int{1, 2, 0},
+		},
+		{
+			//	[0,1,0,3,12]
+			name: "test3",
+			args: args{
+				nums: []int{0, 1, 0, 3, 12},
+			},
+			want: []int{1, 3, 12, 0, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moveZeroes(tt.args.nums)
+
+			if !reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("moveZeroes() = %v, want %v", tt.args.nums, tt.want)
+			}
+		})
+	}
+}
