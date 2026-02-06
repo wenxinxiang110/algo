fe666a5c9c33782f063ddd55f9552e7d897d3339
wenxinxiang <wenxinxiang@tencent.com>
2026-02-06 13:25:43 +0800

    feat(medium.53): maxSubArray 最大子数组和

---
+++ leetcode/editor/cn/53_maximum-subarray_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}},
+			want: 6,
+		}, {
+			name: "test2",
+			args: args{nums: []int{1}},
+			want: 1,
+		}, {
+			name: "test3",
+			args: args{nums: []int{-1}},
+			want: -1,
+		}, {
+			name: "test4",
+			args: args{nums: []int{5, 4, -1, 7, 8}},
+			want: 23,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
