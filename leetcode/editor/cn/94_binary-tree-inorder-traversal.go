5235d9abf2bbad69d77c8ae076294cf842a4ccd5
wenxinxiang <wenxinxiang@tencent.com>
2026-02-21 01:29:29 +0800

    feat(easy.94): inorderTraversal 中序遍历

---
+++ leetcode/editor/cn/94_binary-tree-inorder-traversal.go
@@ -0,0 +1,66 @@
+package main
+
+//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 2380 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	return inorderTraversalRecursive(root)
+}
+
+func inorderTraversalRecursive(root *TreeNode) (result []int) {
+	if root == nil {
+		return
+	}
+	result = append(result, inorderTraversalRecursive(root.Left)...)
+	result = append(result, root.Val)
+	result = append(result, inorderTraversalRecursive(root.Right)...)
+	return result
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
